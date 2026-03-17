61f1b2ff5301cd769e3d6dd00cc07a2995d5b48f
Sindhuja <sindhujakarthikeyan3@gmail.com>
2026-03-17 23:31:29 +0530

    fixed render port issue

--- app.py
+++ app.py
@@ -153,8 +153,11 @@ def diet():
     return jsonify({"diet": diet})
 
 # ------------------ INIT ------------------
+import os
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
